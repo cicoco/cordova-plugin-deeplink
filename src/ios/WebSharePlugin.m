2ff3a5e42dd5bbb4e96ce54cb48eefb0acc7d78d
tafiagu <tafiagu@gmail.com>
2022-12-19 13:27:20 +0800

    调整正则

--- src/ios/WebSharePlugin.m
+++ src/ios/WebSharePlugin.m
@@ -79,7 +79,7 @@
     } else {
         url = url;
     }
-    NSString *urlRegex = @"^((https|http|ftp|rtsp|mms)?://)?(([0-9a-z_!~*\'().&=+$%-]+: )?[0-9a-z_!~*\'().&=+$%-]+@)?(([0-9]{1,3}.){3}[0-9]{1,3}|([0-9a-z_!~*\'()-]+.)*([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].[a-z]{2,6})(:[0-9]{1,4})?((/?)|(/[0-9a-z_!~*\'().;?:@&=+$,%#-]+)+/?)$";
+    NSString *urlRegex = @"^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$";
  
     NSPredicate* urlTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", urlRegex];
  
